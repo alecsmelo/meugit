6183d9d95f762aa92f43c667a7a317e3e405d89a
Alecs Melo <aferreiramelo@gmail.com>
2022-05-31 13:20:40 -0300

    Adicionado Sanatize para conversão da moeda no item Salário e o converso de float no input da informação do mesmo, melhoramentos de código

--- Cadastro.py
+++ Cadastro.py
@@ -3,32 +3,21 @@ class Cadastro():
         self.Nome = Nome
         self.Cargo = Cargo
         self.Turno = Turno
-        self.Salario = Salario
+        self.Salario = float(Salario)
 
     def ExibirCasdastro(self):
-        amount = self.Salario
-        thousands_separator = "."
-        fractional_separator = ","
-
-        currency = "R${:,.2f}".format(amount)
-
-        if thousands_separator == ".":
-            main_currency, fractional_currency = currency.split(".")[0], currency.split(".")[1]
-            new_main_currency = main_currency.replace(",", ".")
-            currency = new_main_currency + fractional_separator + fractional_currency
-
+        Salario_BRL = "R${:,.2f}".format(self.Salario).replace(",","X").replace(".",",").replace("X",".")
         print("Funcionário: " + self.Nome)
         print("Cargo: " + self.Cargo)
         print("Turno: " + self.Turno)
-        print("Salário: " + str(currency))
+        print("Salário: " + str(Salario_BRL))
 
-CadFunc = input("Digite Nome do Funcionário: ")
+CadNome = input("Digite Nome do Funcionário: ")
 CadCargo = input("Digite o Cargo do Funcionário: ")
 CadTurno = input("Digite o Horário: ")
-CadSalario = float(input("Digite o Salário: "))
+CadSalario = input("Digite o Salário: ")
 
-CadFunc = Cadastro(CadFunc, CadCargo, CadTurno, CadSalario)
-print("")
+CadFunc = Cadastro(CadNome, CadCargo, CadTurno, CadSalario)
 CadFunc.ExibirCasdastro()
 
 
